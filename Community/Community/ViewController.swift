70c039977571cea67da5b13a03e75ee03c13ffeb
Pamella <pamella.as.304@gmail.com>
2023-08-16 08:41:40 -0300

    projeto-novo

---
+++ Community/Community/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Community
+//
+//  Created by Pamella Alvarenga on 16/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
