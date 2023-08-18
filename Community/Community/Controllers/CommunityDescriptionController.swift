8cff7024b0e74dc0b6333ac8a0ed8a56b2988fb9
Pamella <pamella.as.304@gmail.com>
2023-08-18 10:22:40 -0300

    added-nav-bar

--- Community/Community/Controllers/CommunityDescriptionController.swift
+++ Community/Community/Controllers/CommunityDescriptionController.swift
@@ -13,22 +13,28 @@ class CommunityDescriptionController: UIViewController {
     @IBOutlet weak var descritivoLabel: UILabel!
     @IBOutlet weak var localLabel: UILabel!
     @IBOutlet weak var descricaoLabel: UILabel!
+    @IBOutlet weak var navBar: UINavigationBar!
+    
     var comunidade: Comunidade?
     
     override func viewDidLoad() {
         configuraView()
         communityImage.layer.cornerRadius = 10
-        communityImage.layer.masksToBounds = true
+        communityImage.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
     
     func configuraView() {
-        guard let comunidade = comunidade else { return }
         
+        
+        guard let comunidade = comunidade else { return }
         
         communityImage.image = UIImage(named: comunidade.image)
         tituloLabel.text = comunidade.name
         localLabel.text = comunidade.location
         descritivoLabel.text = comunidade.tags
+        
+     
+        
     }
     
     
